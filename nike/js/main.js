7ff856fab02347a18891a1863105823dac5b988a
sergo-proggger <sergey.cherokee@gmail.com>
2025-06-28 19:23:42 +0300

    Add files via upload

--- nike/js/main.js
+++ nike/js/main.js
@@ -1,7 +1,32 @@
+// слайдер хедер
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.hero__img');
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active'); 
+        if (i === index) {
+            slide.classList.add('active'); 
+        }
+    });
+}
+
+function changeSlide(index) {
+    currentSlide = index; 
+    showSlide(currentSlide); 
+}
+
+showSlide(currentSlide);
+
+//бургер
+
 document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.nav').classList.toggle('open');
 })
+
+// видео
 
 const video = document.getElementById('myVideo');
 const playButton = document.getElementById('playButton');
@@ -13,7 +38,6 @@ video.addEventListener('pause', () => {
 video.addEventListener('play', () => {
     playButton.style.display = 'none';
 });
-
 playButton.addEventListener('click', () => {
     if (video.paused) {
         video.play();
@@ -23,6 +47,8 @@ playButton.addEventListener('click', () => {
 video.addEventListener('ended', () => {
     playButton.style.display = 'none';
 });
+
+// слайдер отзывы
 
 new Swiper('.testimonials__slider', {
 
