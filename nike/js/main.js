27551c41129ed89af371e787244346838e2fd12b
sergo-proggger <sergey.cherokee@gmail.com>
2025-07-02 09:57:35 +0300

    Add files via upload

--- nike/js/main.js
+++ nike/js/main.js
@@ -26,28 +26,6 @@ document.querySelector('.burger').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('open');
 })
 
-// видео
-
-const video = document.getElementById('myVideo');
-const playButton = document.getElementById('playButton');
-
-video.addEventListener('pause', () => {
-    playButton.style.display = 'block';
-});
-
-video.addEventListener('play', () => {
-    playButton.style.display = 'none';
-});
-playButton.addEventListener('click', () => {
-    if (video.paused) {
-        video.play();
-        playButton.style.display = 'none'; 
-    }
-});
-video.addEventListener('ended', () => {
-    playButton.style.display = 'none';
-});
-
 // слайдер отзывы
 
 new Swiper('.testimonials__slider', {
@@ -62,5 +40,21 @@ new Swiper('.testimonials__slider', {
 
     pagination: {
         el: '.testimonials__pagination',
+    },
+});
+
+// слайдер отзывы 2 
+new Swiper('.testimonials__slider', {
+
+    spaceBetween: 0,
+    slidesPerView: 1,
+
+    navigation: {
+        nextEl: '.testimonials__next',
+        prevEl: '.testimonials__prev',
+    },
+
+    pagination: {
+        el: '.testimonials__pagination2',
     },
 });
