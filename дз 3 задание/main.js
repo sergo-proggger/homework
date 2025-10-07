f7c470bb277a7e0631dc3a78bac1c949245a4289
sergo-proggger <sergey.cherokee@gmail.com>
2025-10-07 23:27:32 +0300

    Add files via upload

---
+++ дз 3 задание/main.js
@@ -0,0 +1,6 @@
+const firstName = "Сергей";
+const lastName = "Черномырдин";
+const age = 23;
+const isStudent = "aroken.ru";
+
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} года. Я ученик курса: ${isStudent}.`); 
---
+++ дз/main.js
@@ -0,0 +1,13 @@
+// задание 1
+const firstName = "Сергей"; 
+const lastName = "Черномырдин"; 
+const isStudent = true; 
+
+// задание 2 
+let age = 23; 
+let currentYear = 2025;
+let birthYear = currentYear - age;
+console.log(birthYear); 
+
+// задание 4 (3 в отдельном файле)
+'123456789false1'
